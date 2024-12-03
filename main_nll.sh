54db56ca4eefa3978131ec41a08dd274e6dcd9fd
hayashi-y-br <hayashi.y.br@m.titech.ac.jp>
2024-12-03 14:45:01 +0900

    Add main*.sh

---
+++ main_nll.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#$ -cwd
+#$ -l gpu_h=1
+#$ -l h_rt=24:00:00
+#$ -t 1-5
+
+eval "$(/apps/t4/rhel9/free/miniconda/24.1.2/bin/conda shell.bash hook)"
+conda activate mil
+
+python main.py -m seed=$(($SGE_TASK_ID-1)) model.activation=relu,exp settings.loss=nll settings.annealing_step=10,20,50,100 settings.lr=0.001,0.0005,0.0001,0.00005,0.00001 settings.reg=0
